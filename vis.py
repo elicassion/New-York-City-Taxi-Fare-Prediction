d552bc7d7c27a216260de7d46eca572abe3918d7
etoss <elicassion@sjtu.edu.cn>
2018-09-07 11:15:47 -0400

    init commit

---
+++ vis.py
@@ -0,0 +1,19 @@
+import pyecharts
+import pandas as pd
+import numpy as np
+
+import os
+import codecs
+
+DATA_DIR = 'data'
+FILE_NAME = 'train.csv'
+INPUT_FILE = os.path.join(DATA_DIR, FILE_NAME)
+
+CHUNKSIZE = 10000
+MAX_ROW_NUM = 50000000
+
+for cur_row_counter in range(0, MAX_ROW_NUM):
+    df = pd.read_csv('data\\train.csv',
+                     head = True,
+                     chunksize = 10000)
+
