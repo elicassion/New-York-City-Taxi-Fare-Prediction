2572bf86ae84fc5ba90fc946c5cad6f5c7b8b47f
etoss <elicassion@sjtu.edu.cn>
2018-09-07 14:13:56 -0400

    process data

--- vis.py
+++ vis.py
@@ -10,10 +10,14 @@ FILE_NAME = 'train.csv'
 INPUT_FILE = os.path.join(DATA_DIR, FILE_NAME)
 
 CHUNKSIZE = 10000
-MAX_ROW_NUM = 50000000
+# MAX_ROW_NUM = 50000000
+MAX_ROW_NUM = 10000
 
-for cur_row_counter in range(0, MAX_ROW_NUM):
+for cur_row_counter in range(0, MAX_ROW_NUM, CHUNKSIZE):
     df = pd.read_csv('data\\train.csv',
                      head = True,
-                     chunksize = 10000)
+                     chunksize = 10000,
+                     )
+
+
 
